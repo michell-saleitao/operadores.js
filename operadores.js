fc1b800a4bebaf92671bb144764e867acaf02f1f
Michell Sá Leitão <michell.leitao@gmail.com>
2022-01-21 11:02:18 -0300

    Novo teste com comparação

--- operadores.js
+++ operadores.js
@@ -3,14 +3,14 @@
 // === igual e do mesmo tipo
 // !== diferente, inclusive do tipo
 
-// console.log(5 > 4) // true
-// console.log(5 >= 4) // true
-// console.log(3 > 4) //false
-// console.log(3 <= 4) //false
+console.log(5 > 4) // true
+console.log(5 >= 4) // true
+console.log(3 > 4) //false
+console.log(3 <= 4) //true
 
 //Sempre salvar depois de alterar
 
-console.log(4 == "4") // True
-console.log(4 === "4") // False
-console.log(4 != "5") // True
-console.log(4 !== "5") // True
+//console.log(4 == "4") // True
+//console.log(4 === "4") // False
+//console.log(4 != "5") // True
+//console.log(4 !== "5") // True
