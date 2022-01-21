4ae53b891abd1583f37dc3d7e90db0ba29d2bdb9
Michell Sá Leitão <michell.leitao@gmail.com>
2022-01-21 10:31:27 -0300

    Início do repositório

---
+++ operadores.js
@@ -0,0 +1,9 @@
+// == igual
+// != diferente
+// === igual e do mesmo tipo
+// !== diferente, inclusive do tipo
+
+console.log(5 > 4) // true
+console.log(5 >= 4) // true
+console.log(3 > 4) //false
+console.log(3 <= 4) //false
