9a8eb28dc54f6a4f742340019f720488d6b399f7
Michell Sá Leitão <michell.leitao@gmail.com>
2022-01-22 11:39:26 -0300

    Criado o arquivo operadores.logicos.js

--- operadores.js
+++ operadores.js
@@ -3,10 +3,10 @@
 // === igual e do mesmo tipo
 // !== diferente, inclusive do tipo
 
-console.log(5 > 4) // true
-console.log(5 >= 4) // true
-console.log(3 > 4) //false
-console.log(3 <= 4) //true
+//console.log(5 > 4) // true
+//console.log(5 >= 4) // true
+//console.log(3 > 4) //false
+//console.log(3 <= 4) //true
 
 //Sempre salvar depois de alterar
 
@@ -14,3 +14,16 @@ console.log(3 <= 4) //true
 //console.log(4 === "4") // False
 //console.log(4 != "5") // True
 //console.log(4 !== "5") // True
+
+//Verificar se a pessoa é maior que 18 anos
+
+const pessoa = "Michell"
+const idade = 39
+const rest = 18
+
+if (idade >= rest) {
+    console.log(`A idade do ${pessoa} é ${idade} anos, portanto ele está autorizado 
+a entrar, visto que o mesmo é maior de ${rest} anos!`)
+} else {
+    console.log("Não Autorizado!")
+}
---
+++ operadores.logicos.js
@@ -0,0 +1,11 @@
+// Operadores Lógicos
+
+// &&
+// ||
+// !
+
+console.log(5 == 5 && 6 == 6) // True
+console.log(5 == 5 || 6 == 6) // True
+console.log(4 == 5 && 6 == 6) // False
+console.log(!(5 == 5) && 6 == 6) // False
+console.log(4 == 5 || 6 == 6) // True
